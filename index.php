38cdb55b06393d510dd056c5c2f4191b2b6aac38
John Edrington <jedrington+git@redhat.com>
2017-08-31 13:29:27 -0400

    First version

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
-<?php
-class IndexController extends Yaf_Controller_Abstract {
-   /* default action */
-   public function indexAction() {
-       $this->_view->word = "hello world";
-       //or
-       // $this->getView()->word = "hello world";
-       if(!$hostname) $hostname = trim(`hostname`); 
-       if(!$hostname) $hostname = exec('echo $HOSTNAME');
-       if(!$hostname) $hostname = preg_replace('#^\w+\s+(\w+).*$#', '$1', exec('uname -a'));       
-       echo $hostname
-   }
-}
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php
+        $hostname = gethostname();
+        echo "<p>Hello! You are connected to $hostname </p>";
+
+        echo "<p>Here are the contents of /etc/resolv.conf, /etc/hosts, and /etc/ntp.conf:</p>";
+
+        echo "-- resolv.conf --<br>";
+        echo nl2br (file_get_contents( "/etc/resolv.conf" ));
+        echo "<br>";
+
+        echo "-- hosts --<br>";
+        echo nl2br ( file_get_contents( "/etc/hosts" ));
+        echo "<br>";
+
+        echo "-- ntp.conf--<br>";
+        echo nl2br (`cat /etc/ntp.conf server | grep ^server`);
+        echo "<br>";
+
+
+ ?>
+ </body>
+</html>
